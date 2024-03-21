51ccc97acbbedcff91d9fe096b725538079e1ce5
Nourchen1507 <nourchen.hedfi@esprit.tn>
2024-03-21 02:58:23 +0100

    log

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 
 
@@ -12,6 +13,8 @@ const routes: Routes = [
   {
     path: "home", component: HomeComponent
   },
+  {path:'login',component:LoginComponent},
+
 ];
 
 @NgModule({
