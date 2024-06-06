516cb906d39140b5646e734482f7be8b6f296e85
Nourchen1507 <nourchen.hedfi@esprit.tn>
2024-06-06 17:19:14 +0100

    test

--- karma.conf.js
+++ karma.conf.js
@@ -1,22 +1,62 @@
 module.exports = function(config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     files: [
-      'src/**/*.js',
-      'test/**/*.spec.js'
+      { pattern: './src/test.ts', watched: false }
     ],
-    exclude: [],
     preprocessors: {
-      'src/**/*.js': ['coverage']
+      './src/test.ts': ['@angular-devkit/build-angular']
     },
-    reporters: ['progress', 'coverage'],
+    browsers: ['Chrome'],
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
     singleRun: false,
-    concurrency: Infinity
+    restartOnFileChange: true,
+    webpack: {
+      mode: 'development',
+      resolve: {
+        extensions: ['.ts', '.js']
+      },
+      module: {
+        rules: [
+          { test: /\.ts$/, loader: '@ngtools/webpack' }
+        ]
+      }
+    },
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // Laissez Jasmine gérer le réglage du contexte
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // Désactive les rapports en double affichés par Jasmine
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage/application'), // Indique à l'outil de génération de rapports où stocker les rapports
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
+    },
+    angularCli: {
+      environment: 'dev'
+    },
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--no-sandbox',
+          '--remote-debugging-port=9222'
+        ]
+      }
+    },
   });
 };
